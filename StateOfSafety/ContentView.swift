8cfe4a78806ecf7e6d2313b89b25a8f06bcdc580
nhi-ngo <nhingo.y@gmail.com>
2025-06-03 16:10:32 -0500

    Initial Commit

---
+++ StateOfSafety/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateOfSafety
+//
+//  Created by Nhi Ngo on 6/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StateOfSafety/StateOfSafetyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateOfSafetyApp.swift
+//  StateOfSafety
+//
+//  Created by Nhi Ngo on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct StateOfSafetyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
