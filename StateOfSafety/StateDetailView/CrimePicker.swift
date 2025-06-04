8ec17773cca6bc00cacbf0088086c13a8a15c8ad
nhi-ngo <nhingo.y@gmail.com>
2025-06-04 17:21:45 -0500

    add crime type picker

---
+++ StateOfSafety/StateDetailView/CrimePicker.swift
@@ -0,0 +1,41 @@
+//
+//  CrimePicker.swift
+//  StateOfSafety
+//
+//  Created by Nhi Ngo on 6/4/25.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct CrimeType: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let abbr: String
+    let code: Int
+}
+
+let crimeTypes: [CrimeType] = [
+    CrimeType(name: "Burglary", abbr: "BUR", code: 60),
+    CrimeType(name: "Robbery", abbr: "ROB", code: 30),
+    CrimeType(name: "Larceny", abbr: "LAR", code: 70)
+]
+
+struct CrimeTypePicker: View {
+    @Binding var selectedCrime: CrimeType
+    
+    var body: some View {
+        Picker("Select Crime Type", selection: $selectedCrime) {
+            ForEach(crimeTypes) { crimeType in
+                Text(crimeType.name).tag(crimeType)
+            }
+            
+        }
+        .pickerStyle(.segmented)
+    }
+}
+#Preview {
+    CrimeTypePicker(selectedCrime: .constant(crimeTypes[0]))
+}
+
